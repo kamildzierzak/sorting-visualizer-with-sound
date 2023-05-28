20e0d938abcce7a555aff7622dedd4bdf6e57b86
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-05-28 22:36:28 +0200

    add possibility to stop visualization, change visualization speed, switch algorithm during visualization

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ let selectedAlgorithm = bubbleSort
 let howManyRandomNumbers = 50
 let arrayOfRandomNumbers = []
 let audioCtx = null
-const duration = 0.1
+let animateDuration = 10
+let animationTimeout = null
+const noteDuration = 0.1
 
 const init = () => {
   arrayOfRandomNumbers = []
@@ -28,6 +30,10 @@ const play = () => {
   animate(moves)
 }
 
+const stop = () => {
+  clearTimeout(animationTimeout)
+}
+
 const animate = moves => {
   if (moves.length === 0) {
     showBars()
@@ -41,16 +47,16 @@ const animate = moves => {
     swap(arrayOfRandomNumbers, i, j)
   }
 
-  playNote(arrayOfRandomNumbers[i] * 500 + 200, duration)
-  playNote(arrayOfRandomNumbers[j] * 500 + 200, duration)
+  playNote(arrayOfRandomNumbers[i] * 500 + 200, noteDuration)
+  playNote(arrayOfRandomNumbers[j] * 500 + 200, noteDuration)
 
   showBars(move)
 
-  setTimeout(() => animate(moves), 25)
+  animationTimeout = setTimeout(() => animate(moves), animateDuration)
 }
 
 const showBars = move => {
-  playground.innerHTML = ''
+  barsContainer.innerHTML = ''
 
   for (let i = 0; i < arrayOfRandomNumbers.length; i++) {
     const bar = document.createElement('div')
@@ -60,7 +66,7 @@ const showBars = move => {
     if (move && move.indicies.includes(i)) {
       bar.style.backgroundColor = move.type === 'swap' ? '#E52B50' : '#4A9976'
     }
-    playground.appendChild(bar)
+    barsContainer.appendChild(bar)
   }
 }
 
@@ -74,8 +80,10 @@ const showAlgorithmsOptions = () => {
   })
 }
 
-const selectAlgorithm = () =>
-  (selectedAlgorithm = algorithms[selectButton.value])
+const selectAlgorithm = () => {
+  selectedAlgorithm = algorithms[selectButton.value]
+  stop()
+}
 
 const playNote = (frequency, duration) => {
   if (!audioCtx) {
@@ -99,25 +107,40 @@ const playNote = (frequency, duration) => {
   node.connect(audioCtx.destination)
 }
 
-const playground = document.querySelector('#playground')
+const barsContainer = document.querySelector('#barsContainer')
 
 const selectButton = document.querySelector('#algorithm-select')
 const rangeInput = document.querySelector('#powerRanger')
-const selectedValueText = document.querySelector('#quantity')
+const rangeValueText = document.querySelector('#quantity')
+const speedInput = document.querySelector('#speedRanger')
+const speedValueText = document.querySelector('#speed')
 const generateButton = document.querySelector('#generate')
 const playButton = document.querySelector('#play')
+const stopButton = document.querySelector('#stop')
 
 selectButton.addEventListener('change', selectAlgorithm)
+
 rangeInput.addEventListener('input', e => {
-  const selectedValue = rangeInput.value
+  let selectedValue = rangeInput.value
   howManyRandomNumbers = selectedValue
-  selectedValueText.innerText = selectedValue
+  rangeValueText.innerText = selectedValue
 })
 rangeInput.addEventListener('change', e => {
+  stop()
   init()
 })
+
+speedInput.addEventListener('input', e => {
+  let selectedValue = speedInput.value
+  animateDuration = selectedValue
+  speedValueText.innerText = selectedValue
+})
+
 generateButton.addEventListener('click', init)
+
 playButton.addEventListener('click', play)
+
+stopButton.addEventListener('click', stop)
 
 init()
 showAlgorithmsOptions()
